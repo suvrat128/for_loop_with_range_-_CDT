c153876b7decf654889c054241e52d3feacf9f49
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-12 22:34:47 +0530

    Update 08_sum_of_evendig_evenip.py

--- 08_sum_of_evendig_evenip.py
+++ 08_sum_of_evendig_evenip.py
@@ -7,6 +7,14 @@ for ip in range(len(s)):
         summ+=int(s[ip])
 print(summ)
 
+
+#the optimize code:
+s = input()
+summ = 0
+for ip in range(1,len(s),2):
+    if s[ip] in '24680':
+        summ+=int(s[ip])
+print(summ)
 '''
 
 '''
