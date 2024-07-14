efb09acfcd3b73d5c1c0f19011264b5f510bff44
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-14 21:05:11 +0530

    Update 09_reverse_str_witout_slacing.py

--- 09_reverse_str_witout_slacing.py
+++ 09_reverse_str_witout_slacing.py
@@ -7,6 +7,36 @@ for i in s:
     reverse = i+reverse
 print(reverse)
 
+'''
+process:
+1. take input from user
+3 string = 'happy'
+2. take a variabe with empty string where we store the reversed string
+3. using loop:
+    iteration 1:
+        it will fetch 'h' so i  is 'h'
+        we will add in start of the 'h' to variable reverse 
+        reverse='h'
+    iteration 2:
+        it will fetch 'a' so i  is 'a'
+        we will add in start of the 'a' to variable reverse 
+        reverse='ah'
+    iteration 3:
+        it will fetch 'p' so i  is 'p'
+        we will add in start of the 'p' to variable reverse 
+        reverse='pah'
+    iteration 4:
+        it will fetch 'p' so i  is 'p'
+        we will add in start of the 'p' to variable reverse 
+        reverse='ppah'
+    iteration 5:
+        it will fetch 'y' so i  is 'y'
+        we will add in start of the 'y' to variable reverse 
+        reverse='yppah'
+5. at last after complatioon of lopp we print reverse = 'yppah'
+
+'''
+
 # another way to reverse using indexing
 
 s= input()
@@ -14,3 +44,11 @@ reverse = ''
 for ip in range(-1,-(len(s)+1),-1):
     reverse+=s[ip]
 print(reverse)
+
+'''
+process:
+1. take input from user
+3 string = 'happy'
+2. take a variabe with empty string where we store the reversed string
+3. using loop:
+'''
