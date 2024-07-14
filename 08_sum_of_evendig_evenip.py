914515f295679e48bf48be8a21d29fdcc25fe44b
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-14 20:51:19 +0530

    Update 08_sum_of_evendig_evenip.py

--- 08_sum_of_evendig_evenip.py
+++ 08_sum_of_evendig_evenip.py
@@ -16,5 +16,21 @@ for ip in range(1,len(s),2):
         summ+=int(s[ip])
 print(summ)
 '''
+# string = h2a4py
+1. take input from user
+2. asumming there is no even digit present at odd indexposition so summ = 0
+3. using loop
+    iteration 1:
+        it will fetch 1 index position so ip = 1
+        check s[1] = '2' is in '24680' true:
+            summ = 0+ 2 
+    iteration 2:
+        it will fetch 3 indexposition so ip = 3
+        check s[3] = '4' is in '24680' true:
+            summ = 2+4
+    iteration 3:
+        it will fetch 5 indexposition so ip = 5
+        check s[5] = 'y' is in '24680' false: so summ still 6
+4. at last after completion of loop print the value of summ =6
 
 '''
